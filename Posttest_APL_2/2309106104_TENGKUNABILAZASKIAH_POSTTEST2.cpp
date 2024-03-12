d7bad66fbcc87c26d56dd817710bfb77b7bb4b74
tengkunabilazaskiah <162078822+tengkunabilazaskiah@users.noreply.github.com>
2024-03-12 12:32:36 +0800

    Add files via upload

---
+++ Posttest_APL_2/2309106104_TENGKUNABILAZASKIAH_POSTTEST2.cpp
@@ -0,0 +1,207 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Fungsi login
+int login() {
+    int loginAttempt = 0;
+    string nama, nim;
+
+    // Login
+    while (loginAttempt < 3) {
+        cout << "Masukkan Nama Anda: ";
+        cin >> nama;
+        cout << "Masukkan NIM Anda: ";
+        cin >> nim;
+
+        // Simulasi login (contoh: nama = "bila", nim = "104")
+        if (nama == "bila" && nim == "104") {
+            cout << "Login berhasil! Welcome!, " << nama << "!" << endl;
+            return 1;
+        } else {
+            cout << "Login gagal. Silakan coba lagi." << endl;
+            loginAttempt++;
+        }
+    }
+
+    if (loginAttempt >= 3) {
+            cout << "Anda telah gagal login sebanyak 3 kali. Mianhaeyo Program berhenti." << endl;
+    return 0;
+}
+}
+
+// Deklarasi Struct DataTugas
+struct DataTugas {
+    string nama_matkul;
+    string deskripsi_tugas;
+    string deadline;
+    int status; //( 0: Belum Dikerjakan, 1: Sedang Dikerjakan, 2: Selesai)
+};
+
+// Deklarasi Status Pengerjaan Tugas 
+const int STATUS_BELUM_DIKERJAKAN = 0;
+const int STATUS_SEDANG_DIKERJAKAN = 1;
+const int STATUS_SELESAI = 2;
+
+// Deklarasi Array dataTugas
+DataTugas dataTugas[100]; // Maksimal 100 tugas
+
+
+// Fungsi Untuk Menambahkan Tugas
+void tambahTugas() {
+    string nama_matkul, deskripsi_tugas, deadline;
+    int status;
+
+    cout << "**Tambah Tugas**" << endl;
+
+    cout << "Masukkan nama mata kuliah: ";
+    cin >> nama_matkul;
+
+    cout << "Masukkan deskripsi tugas: ";
+    cin.ignore();
+    getline(cin, deskripsi_tugas);
+
+    cout << "Masukkan deadline tugas (tanggal,bulan): ";
+    cin >> deadline;
+
+    cout << "Masukkan status tugas (0: Belum Dikerjakan, 1: Sedang Dikerjakan, 2: Selesai): ";
+    cin >> status;
+
+    // Mencari Index Kosong Di Array
+    int i = 0;
+    while (dataTugas[i].nama_matkul != "") {
+        i++;
+    }
+
+    // Menambahkan Data Tugas Ke Array
+    dataTugas[i].nama_matkul = nama_matkul;
+    dataTugas[i].deskripsi_tugas = deskripsi_tugas;
+    dataTugas[i].deadline = deadline;
+    dataTugas[i].status = status;
+
+    cout << "Tugas berhasil ditambahkan!" << endl;
+}
+
+// Fungsi Untuk Melihat Daftar Tugas
+void lihatDaftarTugas() {
+    cout << "**Daftar Tugas**" << endl;
+
+    for (int i = 0; i < 100; i++) {
+        if (dataTugas[i].nama_matkul != "") {
+            cout << "-------------------------" << endl;
+            cout << "Nama Mata Kuliah: " << dataTugas[i].nama_matkul << endl;
+            cout << "Deskripsi Tugas: " << dataTugas[i].deskripsi_tugas << endl;
+            cout << "Deadline: " << dataTugas[i].deadline << endl;
+            cout << "Status: ";
+            if (dataTugas[i].status == 0) {
+                cout << "Belum Dikerjakan" << endl;
+            } 
+            else if (dataTugas[i].status == STATUS_SEDANG_DIKERJAKAN) {
+                cout << "Sedang Dikerjakan" << endl;
+            } else {
+                cout << "Selesai" << endl;
+            }
+        }
+    }
+}
+
+// Fungsi untuk mengubah status tugas
+void ubahStatusTugas() {
+    int id_tugas;
+
+    cout << "**Ubah Status Tugas**" << endl;
+    cout << "Masukkan ID tugas: ";
+    cin >> id_tugas;
+
+    // Mencari Data Tugas Dengan ID Yang Diberikan
+    int i = 0;
+    while (dataTugas[i].nama_matkul != "" && i < 100) {
+        if (i == id_tugas) {
+            break;
+        }
+        i++;
+    }
+
+    if (dataTugas[i].nama_matkul == "") {
+        cout << "ID tugas tidak ditemukan!" << endl;
+        return;
+    }
+
+    cout << "Status tugas saat ini: ";
+    if (dataTugas[i].status == STATUS_BELUM_DIKERJAKAN) {
+        cout << "Belum Dikerjakan" << endl;
+    } else if (dataTugas[i].status == STATUS_SEDANG_DIKERJAKAN) {
+        cout << "Sedang Dikerjakan" << endl;
+    } else {
+        cout << "Selesai" << endl;
+    }
+
+    cout << "Masukkan status baru (0: Belum Dikerjakan, 1: Sedang Dikerjakan, 2: Selesai): ";
+    cin >> dataTugas[i].status;
+
+    // Validasi Input Status Baru
+    if (dataTugas[i].status < STATUS_BELUM_DIKERJAKAN || dataTugas[i].status > STATUS_SELESAI) {
+        cout << "Status baru tidak valid!" << endl;
+        return;
+    }
+    cout << "Status tugas berhasil diubah!" << endl;
+}
+
+// Fungsi untuk menghapus tugas berdasarkan indeks data
+void hapusTugas() {
+    int id_tugas;
+    cout << "**Hapus Tugas**" << endl;
+    cout << "Masukkan ID tugas: ";
+    cin >> id_tugas;
+
+    if (id_tugas < 0 || id_tugas >= 100 || dataTugas[id_tugas].nama_matkul == "") {
+        cout << "Indeks tugas tidak valid atau tidak ditemukan!" << endl;
+        return;
+    }
+
+    cout << "Tugas pada indeks " << id_tugas << " berhasil dihapus!" << endl;
+    dataTugas[id_tugas].nama_matkul = "";
+    dataTugas[id_tugas].deskripsi_tugas = "";
+    dataTugas[id_tugas].deadline = "";
+    dataTugas[id_tugas].status = STATUS_BELUM_DIKERJAKAN;
+}
+
+int main() {
+    if (!login()) { // Menjalankan login function
+        return 1; // Jika login gagal, program akan berhenti
+    }
+    // Fungsi Untuk Menampilkan Menu
+    int menu;
+    while (true) {
+        cout << "**!!WELCOME DI JOKI TUGAS!!**" << endl;
+        cout << "1. Tambah Tugas" << endl;
+        cout << "2. Lihat Daftar Tugas" << endl;
+        cout << "3. Ubah Status Tugas" << endl;
+        cout << "4. Hapus Tugas" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Pilih menu: ";
+        cin >> menu;
+        switch (menu) {
+            case 1:
+                tambahTugas();
+                break;
+            case 2:
+                lihatDaftarTugas();
+                break;
+            case 3:
+                ubahStatusTugas();
+                break;
+            case 4:
+                hapusTugas();
+                break;
+            case 5:
+                cout << "Terima kasih telah menggunakan program joki tugas ini!" << endl;
+                return 0;
+            default:
+                cout << "Pilihan tidak valid, silakan coba lagi." << endl;
+                break;
+        }
+    }
+    return 0;
+}
